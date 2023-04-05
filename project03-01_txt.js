b68570180a01e24bf1e6e9de5d864bbf5ca95149
Landen Ingerslev <129755297+lkingerslev@users.noreply.github.com>
2023-04-05 15:16:16 +0000

    Initial commit

---
+++ project03-01_txt.js
@@ -0,0 +1,21 @@
+/*    JavaScript 7th Edition
+  Chapter 3
+  Project 03-01
+
+  Application to calculate total order cost
+  Author: Landen Ingerslev
+  Date:   4/5/2023
+
+  Filename: project03-01.js
+*/
+
+let menuItems = document.getElementsByClassName("menuItems");
+
+for(let i=0;i<menuItems.length;i++){
+  
+}
+
+// Function to display a numeric value as a text string in the format $##.## 
+function formatCurrency(value) {
+  return "$" + value.toFixed(2);
+}
